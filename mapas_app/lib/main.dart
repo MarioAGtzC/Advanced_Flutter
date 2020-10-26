2be739082347fa09b4fa682147d617b3f71c7b80
Marioagtzc <marioagtzc@hotmail.com>
2020-10-26 17:01:20 -0600

    bloc maps

--- mapas_app/lib/main.dart
+++ mapas_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mapas_app/src/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
 import 'package:mapas_app/src/pages/mapa_page.dart';
 import 'package:mapas_app/src/pages/loading_page.dart';
@@ -9,16 +11,21 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      // home: LoadingPage(),
-      home: AccesoGpsPage(),
-      routes: {
-        'mapa': (_) => MapaPage(),
-        'loading': (_) => LoadingPage(),
-        'acceso_gps': (_) => AccesoGpsPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => MiUbicacionBloc())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        // home: LoadingPage(),
+        home: AccesoGpsPage(),
+        routes: {
+          'mapa': (_) => MapaPage(),
+          'loading': (_) => LoadingPage(),
+          'acceso_gps': (_) => AccesoGpsPage(),
+        },
+      ),
     );
   }
 }
